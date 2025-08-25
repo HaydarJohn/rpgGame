1cb4c1c743bf03fa3b092a7961eba897f4430c46
haydarjohn <haydarcan_2003@hotmail.com>
2025-08-25 22:34:26 +0300

    create triangle

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ All rights reserved.
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<iostream>
+#include<fstream>
 
 // #define WINDOW_WIDTH 1920
 // #define WINDOW_HEIGHT 1080
@@ -232,7 +233,124 @@ int main()
 
     // register resizeCallback to glfw
     glfwSetFramebufferSizeCallback(window, resizeCallback);
+
+
     
+    
+    // Start of triangle code.
+    
+    //for debugging purpose
+    int success;
+    char infoLog[512];
+    
+    
+    const char *vertexShaderSource = "#version 460 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    
+    glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+    
+    if(!success)
+    {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
+    infoLog << std::endl;
+    }
+
+
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()"
+        "{"
+        "FragColor = vec4(0.412f, 0.941f, 0.0f, 1.0f);"
+        "}";
+
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,&success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" <<
+        infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram,vertexShader);
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" <<
+        infoLog << std::endl;
+    }
+
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    // start of vertex data
+
+    float vertices[]
+    {
+        -0.5f,-0.5f,0.0f,
+        0.5f,-0.5f,0.0f,
+        0.0f,0.5f,0.0f
+    };
+
+    /*  Warning: Educational
+        So i get same error by not getting any triangles. this time what i had
+        is i accidently wrote glGenBuffers for VAO instead of glGenVertexArrays.
+        but steps are for creating triangle
+        1- create shaders , compile and link shaders
+        2- create vertex array and bind it (bind it where i dont know???)
+        3- create vertex buffer and bind it and attach data
+        4- specify what is in the vertex buffer with vertexattribpointer
+        5- enable our vertex array ???? not sure
+        6- tell in main loop to use our shader
+        7- bind our vertex array again ????? whaaat
+        8- tell our graphics card to draw everything now.
+    */
+    unsigned int VAO,VBO;
+    glGenBuffers(1,&VBO);
+    glGenVertexArrays(1,&VAO);
+
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+
+
+
+   
+    
+
+
 
     // Engine Loop
     while (!glfwWindowShouldClose(window))
@@ -243,6 +361,11 @@ int main()
         glClearColor(0.537f,0.812f,0.941f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+
+        // our render code
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);     // Idk we keep binding same array.
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
         // end of render code 
@@ -269,3 +392,5 @@ void processInput(GLFWwindow* window)
         glfwSetWindowShouldClose(window,true);
     }
 }
+
+
